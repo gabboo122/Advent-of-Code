afa0c15a944e12c7c564efc702039af697985aed
gabboo122 <165178151+gabboo122@users.noreply.github.com>
2025-12-04 15:31:03 +0100

    Creation of repository

---
+++ AdventOfCode.py
@@ -0,0 +1,109 @@
+## Advent of Code
+def d1one():
+    n = 50  # Starting position
+    c = 0   # Count of times at 0 after rotations
+    
+    with open("inputd1.txt") as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        direction, steps = line.strip()[0], int(line.strip()[1:])
+        
+        if direction == 'R':
+            n = (n + steps) % 100
+        elif direction == 'L':
+            n = (n - steps + 100) % 100
+        
+        if n == 0:
+            c += 1
+    
+    return c
+
+def d1two():
+    n = 50
+    c = 0
+    
+    with open("inputd1.txt") as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        direction, steps = line.strip()[0], int(line.strip()[1:])
+        
+        if direction == 'R':
+            # Count how many times we pass 0 moving right
+            for _ in range(steps):
+                n = (n + 1) % 100
+                if n == 0:
+                    c += 1
+        elif direction == 'L':
+            # Count how many times we pass 0 moving left  
+            for _ in range(steps):
+                n = (n - 1 + 100) % 100
+                if n == 0:
+                    c += 1
+    
+    return c
+
+def d2one():
+    n = 0
+    t = 0
+    l = 0
+    with open("inputd2.txt") as f:
+        linesr = f.readlines()
+    ranges = linesr[0]
+    rangesl = []
+    while "," in ranges:
+        first, _, rest = ranges.partition(",")
+        rangesl.append(first)
+        ranges = rest
+
+    for rangen in rangesl:
+        first, _, second = rangen.partition("-")
+        fID = int(first)
+        sID = int(second)
+        
+        for i in range(fID, sID + 1):
+            if len(str(i)) % 2 == 0:
+                l = len(str(i)) // 2
+                first_half = str(i)[:l]
+                second_half = str(i)[l:]
+                if first_half == second_half:
+                    n += 1
+                    t += i
+    return t
+
+def d2two():
+    t = 0
+    l = []
+    with open("inputd2.txt") as f:
+        linesr = f.readlines()
+    ranges = linesr[0]
+    rangesl = []
+    while "," in ranges:
+        first, _, rest = ranges.partition(",")
+        rangesl.append(first)
+        ranges = rest
+
+    for rangen in rangesl:
+        first, _, second = rangen.partition("-")
+        fID = int(first)
+        sID = int(second)
+        
+        for i in range(fID, sID + 1):
+            if len(str(i)) % 2 == 0:
+                l = len(str(i)) // 2
+                first_half = str(i)[:l]
+                second_half = str(i)[l:]
+                if first_half == second_half:
+                    n += 1
+                    t += i
+            ## pour un nombre n de répétitions
+            else:
+                for r in range(max( len(first), len(second)) + 1):
+                    if len(str(i)) % r == 0:
+                        rep = len(str(i)) // r
+                        if str(i) == str(i)[:r] * rep:
+                            n += 1
+                            t += i
+
+    return t
